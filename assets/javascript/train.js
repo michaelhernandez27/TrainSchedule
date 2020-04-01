20b640a09ff4b245ccbcbfe2ac8c86afad4fab26
Michael Hernandez <michael.hernandez27@outlook.com>
2020-04-01 15:57:31 -0400

    edited javascript and just need to get new train written in the table

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -47,3 +47,50 @@ $("#submit").on("click", function(event) {
 
 
 
+database.ref().on("child_added", function(childSnapShot, prevChildKey) {
+
+console.log(childSnapShot.val());
+
+var trainName = childSnapShot.val().name;
+var trainDest = childSnapShot.val().dest;
+var trainTime = childSnapShot.val().time;
+var trainRate = childSnapShot.val().rate;
+trainRate = parseInt(trainRate);
+
+
+console.log("Name: ", trainName);
+console.log("Destination: ", trainDest);
+console.log("Departure: ", trainTime);
+console.log("Frequency: ", trainRate);
+
+
+var firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+console.log("Time Converted: " + firstTimeConverted);
+
+
+var diffTime = moment.duration(moment().diff(moment(trainTime, "HH:mm")), "milliseconds").asMinutes();
+console.log("Difference in Time" + diffTime);
+
+
+var timeRemaining = trainRate - (Math.floor(diffTime) % trainRate);
+console.log(timeRemaining);
+
+
+var nextTrain = diffTime > 0 ? moment().add(timeRemaining, 'minutes' ) : moment(trainTime, "HH:mm") ;
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
+
+  
+var minTilTrain = Math.ceil(moment.duration(moment(nextTrain).diff(moment()), 'milliseconds').asMinutes());
+console.log("MINUTES TILL TRAIN: " + minTilTrain);
+
+
+nextTrain = moment(nextTrain).format("HH:mm");
+
+$("#traintimes > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainRate + "</td><td>" + nextTrain + "</td><td>" + minTilTrain + "</td></tr>");
+
+  
+
+});
+
+
+
